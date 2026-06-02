aca7d2b06e9c7e6206ba3070088b3aa0c262ccec
Ritik Kumar <ritik2900kumar@gmail.com>
2026-06-02 19:04:27 +0530

    added build prompt functionality

---
+++ llm/prompts.py
@@ -0,0 +1,17 @@
+from utils import load_file
+from pathlib import Path
+
+
+PROMPTS_DIR = Path(__file__).parent / "prompts"
+def load_prompt(filepath: str) -> str:
+    return load_file(PROMPTS_DIR/filepath)
+
+
+
+
+PROMPT = load_prompt("react.md")
+
+def generate_prompt(message: str) -> str:
+
+    template = PROMPT
+    return f"{template}\n\n userInput:{message.strip()}"
